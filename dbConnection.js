f517121f2dee39712f2a527007945e4a440902ea
Shruti-Hanchate <65779533+Shruti-Hanchate@users.noreply.github.com>
2020-12-08 16:06:47 +0530

    Delete dbConnection.js

--- dbConnection.js
+++
@@ -1,9 +0,0 @@
-var mysql= require('mysql');
-
-var connection= mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password:'',
-    database: 'novodhiform'
-});
-module.exports=connection;
